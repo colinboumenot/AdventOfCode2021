fde4f0222fa046fb063ba8a3b9eccc025d8a30c9
Christopher Boumenot <boumenot@gmail.com>
2023-04-21 17:36:34 -0700

    Day 22 Complete Tutorial Used

--- day22.py
+++ day22.py
@@ -20,5 +20,60 @@ for line in text:
 
 print(len(cubes))
 
+def intersection(x1, x2, x3, x4):
+    return x1 <= x3 <= x2 or x1 <= x4 <= x2 or x3 <= x1 <= x4 or x3 <= x2 <= x4
+
+def get_intersection(x1, x2, x3, x4):
+    if x3 < x1:
+        return_x = x1
+    else:
+        return_x = x3
+
+    if x4 < x2:
+        return_y = x4
+    else:
+        return_y = x2
+
+    return return_x, return_y
+
+class Cube:
+
+    def __init__(self, x1, x2, y1, y2, z1, z2):
+        self.x1 = x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+        self.z1 = z1
+        self.z2 = z2
+        self.off = []
+
+    def intersecting(self, adjacent):
+        return intersection(self.x1, self.x2, adjacent.x1, adjacent.x2) and intersection(self.y1, self.y2, adjacent.y1, adjacent.y2) and intersection(self.z1, self.z2, adjacent.z1, adjacent.z2)
+
+    def subtraction(self, adjacent):
+        if self.intersecting(adjacent):
+            x = get_intersection(self.x1, self.x2, adjacent.x1, adjacent.x2)
+            y = get_intersection(self.y1, self.y2, adjacent.y1, adjacent.y2)
+            z = get_intersection(self.z1, self.z2, adjacent.z1, adjacent.z2)
+
+            for w in self.off:
+                w.subtraction(adjacent)
+            self.off.append(Cube(x[0], x[1], y[0], y[1], z[0], z[1]))
+
+    def volume(self):
+        return (self.x2 - self.x1 + 1) * (self.y2 - self.y1 + 1) * (self.z2 - self.z1 + 1) - sum([c.volume() for c in self.off])
+
+text_two = open('InputTxt/day22input.txt').read().split('\n')
+cubes_x = []
+for line in text_two:
+    (x1, x2, y1, y2, z1, z2) = tuple(map(int, re.findall(r'-?\d+', line)))
+
+    cube = Cube(x1, x2, y1, y2, z1, z2)
+    for c in cubes_x:
+        c.subtraction(cube)
+    if 'on' in line:
+        cubes_x.append(cube)
+print(sum([c.volume() for c in cubes_x]))
+
 
 
